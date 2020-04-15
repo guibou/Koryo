fa70478ab9698f3a75b67ab5201bb5407dff9d1a
Guillaume Bouchard <guillaume.bouchard@tweag.io>
2020-04-15 08:08:27 +0200

    Update reflex platform and tune the cabal file for ghcjs

--- reflex.nix
+++ reflex.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
-  url = https://github.com/reflex-frp/reflex-platform/archive/500b110820338c921f996e8440d6fae1b2907113.tar.gz;
-  sha256 = "1bjifn5jkb9dmpj6dqs5lhj6i997b2y62ccdz6sh0m5xclw4i6ss";
+  url = https://github.com/reflex-frp/reflex-platform/archive/58873162d542c14cf198e9711986c046b06a18da.tar.gz;
+  sha256 = "1bik0f23n0kvv8205iz61574jlybzqiz6c814ci5l35ablqs66ll";
 })
